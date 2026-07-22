236be15bc2dc611cb1b27ff8b50891b95a1383e5
Fabio <fabio9384@gmail.com>
2026-07-22 01:53:03 +0000

    feat: setup taillwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,17 @@
-<h1> Olá, Mundo! </h1>
-
-<p>
-    Olá {{ $name }}
-</p>
-
-<p>
-    Seus hábitos são:
-</p>
-
-<ul>
-    @foreach ($habits as $habit)
-    <li>
-        {{ $habit }}
-    </li>
-    @endforeach
-</ul>
-
-@auth
-    <p>
-        Você está logado!
-    </p>
-@endauth
-
-@guest
-    <p>
-        Você não está logado!
-    </p>
-@endguest
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>
+            {{ config('app.name') }}
+        </title>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <h1 class="text-3x1 font-bold underline text-gray-500">
+            Hello World!
+        </h1>
+    </body>
+</html>
